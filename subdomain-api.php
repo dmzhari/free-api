418c596a2cf15a01a523cdbea7ae15b5cb24eff6
Dmz Hari <45622512+dmzhari@users.noreply.github.com>
2021-04-04 20:43:34 +0700

    Update subdomain-api.php

--- subdomain-api.php
+++ subdomain-api.php
@@ -1,84 +1,35 @@
 <?php
 
 header('Content-type: application/json');
-ob_clean();
-error_reporting(0);
-
-function getpage($from, $page)
+ob_flush();
+clearstatcache();
+// include '../random-useragent.php';
+function curl($url)
 {
-	$i = 1;
-	if ($page >= 51) {
-		$api['error'] = 'Max Page Is 50!!';
-		echo json_encode($api);
-	} else {
-		while ($i <= $page) {
-			$getpages = "$from/page=$i";
-			echo getlistdomain(file_get_contents($getpages));
-			$i++;
-		}
-	}
+    $setopt = array(
+        CURLOPT_URL             => 'https://osint.sh/subdomain/',
+        CURLOPT_RETURNTRANSFER  => true,
+        CURLOPT_POST            => true,
+        CURLOPT_POSTFIELDS      => "domain=$url",
+        CURLOPT_USERAGENT       => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36',
+        CURLOPT_TIMEOUT         => 60,
+        CURLOPT_CONNECTTIMEOUT  => 60
+    );
+    $ch = curl_init();
+    curl_setopt_array($ch, $setopt);
+    $exe = curl_exec($ch);
+    curl_close($ch);
+    return $exe;
 }
-
-function getlistdomain($url)
-{
-	$scrap = preg_match_all("/<td>(.*?)([a-z])<\/td>/i", $url, $page);
-	$rep = str_replace('</td>', '', $page[0]);
-	$rep = str_replace('<td>', '', $rep);
-	$rep = preg_replace("/\/[^\/]*\/?$/i", '', $rep);
-	$rep = array_filter(array_unique($rep));
-	foreach ($rep as $key) {
-		if (is_null($key)) {
-			$api['status'] = 'failed';
-			$api['result'] = 'No Result!!';
-			echo json_encode($api, JSON_PRETTY_PRINT);
-		} else {
-			$api['status'] = 'success';
-			$api['result'] = $key;
-			echo json_encode($api, JSON_PRETTY_PRINT);
-		}
-	}
-}
-
-$from 		= htmlspecialchars($_GET['from']);
-$page 		= htmlspecialchars($_GET['page']);
-$attacker 	= htmlspecialchars($_GET['nickname']);
-$team 		= htmlspecialchars($_GET['team']);
-
-if (!empty($from) && !empty($page) || !empty($attacker) || !empty($team)) {
-	if ($from == 'archive') {
-		$url = 'https://zone-xsec.com/archive';
-		echo getpage($url, $page);
-	} else if ($from == 'special') {
-		$url = 'https://zone-xsec.com/special';
-		echo getpage($url, $page);
-	} else if ($from == 'onhold') {
-		$url = 'https://zone-xsec.com/onhold';
-		echo getpage($url, $page);
-	} else if ($from == 'attacker') {
-		if (empty($attacker)) {
-			echo 'Nickname attacker null or empty!!';
-		}
-		$name = str_replace('+', '%20', $attacker);
-		$url = "https://zone-xsec.com/archive/attacker/$name";
-		echo getpage($url, $page);
-	} else if ($from == 'team') {
-		if (empty($team)) {
-			echo 'Team attacker null or empty!!';
-		}
-		$name = str_replace('+', '%20', $team);
-		$url = "https://zone-xsec.com/archive/team/$name";
-		echo getpage($url, $page);
-	}
-} else if (empty($from)) {
-	echo "Grabber from empty or null!!<br>";
-	echo 'How To Use : http://localhost/xsec-api.php?from=grab-from<br><br>
-	Grabber From Archive : http://localhost/xsec-api.php?from=archive<br>
-	Grabber From Special : http://localhost/xsec-api.php?from=special<br>
-	Grabber From Onhold	 : http://localhost/xsec-api.php?from=onhold<br><br>
-	Use Grab From Team Or Attacker :<br>
-	http://localhost/xsec-api.php?from=grab-from&page=number-page&team=nickname-team<br>
-	http://localhost/xsec-api.php?from=grab-from&page=number-page&attacker=attacker-name';
-} else if (empty($page)) {
-	echo 'Page null or empty!!<br>';
-	echo 'How To Use : http://localhost/xsec-api.php?from=grab-from&page=number-page';
+if (!empty($_GET['domain'])) {
+    $domain = htmlspecialchars($_GET['domain']);
+    $subdomain = curl($domain);
+    $scrap = preg_match_all("/<a href=\"https:\/\/(.*?)\"/i", $subdomain, $subdo);
+    $rep = array_unique(array_filter(preg_replace("#twitter.com/secgron|teguh.co/|www.linkedin.com/in/secgron|fb.me/secgron|fb.me/secgron|github.com/secgron#", '', $subdo[1])));
+    $api['status'] = http_response_code();
+    $api['result'] = $rep;
+    echo json_encode($api, JSON_PRETTY_PRINT);
+} else {
+    $api['error'] = 'Not Found';
+    echo json_encode($api, JSON_PRETTY_PRINT);
 }
