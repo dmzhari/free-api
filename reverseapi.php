798587c26083c306f805f751531cce1af200820e
Dmz Hari <45622512+dmzhari@users.noreply.github.com>
2021-04-04 20:40:04 +0700

    Update reverseapi.php

--- reverseapi.php
+++ reverseapi.php
@@ -6,6 +6,7 @@
 / # http://example.com/reverseapi.php?domain=your domain
 / # http://example.com/reverseapi.php?ip=your ip
 */
+header('Content-type: application/json');
 set_time_limit(60);
 function reverse($url)
 {
@@ -34,39 +35,41 @@ if (!empty($_GET['domain'])) {
 	$ip = gethostbyname($www);
 	$reverse = reverse($ip);
 	$list = preg_match_all("/<td data-th=\"Domain\">\s(.*?)<\/td>/i", $reverse, $listdomain);
+	$getdomain = str_replace(' ', '', $listdomain[1]);
 	if ($listdomain[1] == true) {
 		$arr = [
 			'author' => './EcchiExplot',
 			'status' => 'success',
-			'result' => $listdomain[1]
+			'result' => array_filter($getdomain)
 		];
-		echo json_encode($arr);
+		echo json_encode($arr, JSON_PRETTY_PRINT);
 	} else {
 		$arr = [
 			'author' => './EcchiExplot',
 			'status' => 'failed',
 			'result' => $listdomain[1]
 		];
-		echo json_encode($arr);
+		echo json_encode($arr, JSON_PRETTY_PRINT);
 	}
 } else if (!empty($_GET['ip'])) {
 	$ip = htmlspecialchars($_GET['ip']);
 	$reverse = reverse($ip);
 	$list = preg_match_all("/<td data-th=\"Domain\">\s(.*?)<\/td>/i", $reverse, $listdomain);
+	$getdomain = str_replace(' ', '', $listdomain[1]);
 	if ($listdomain[1] == true) {
 		$arr = [
 			'author' => './EcchiExplot',
 			'status' => 'success',
-			'result' => $listdomain[1]
+			'result' => array_filter($getdomain)
 		];
-		echo json_encode($arr);
+		echo json_encode($arr, JSON_PRETTY_PRINT);
 	} else {
 		$arr = [
 			'author' => './EcchiExplot',
 			'status' => 'failed',
-			'result' => $listdomain[1]
+			'result' => $getdomain
 		];
-		echo json_encode($arr);
+		echo json_encode($arr, JSON_PRETTY_PRINT);
 	}
 } else {
 	echo "Domain Or Ip Empty";
