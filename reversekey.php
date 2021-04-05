16c25d1dd8a7ebfe4aad563453e8bbf8437a23cb
Dmz Hari <45622512+dmzhari@users.noreply.github.com>
2021-04-05 09:37:04 +0700

    Add files via upload

---
+++ reversekey.php
@@ -0,0 +1,43 @@
+<?php
+
+//error_reporting(0);
+header('Content-type: application/json');
+
+function curl($keyword)
+{
+    $setopt = array(
+        CURLOPT_URL => 'https://osint.sh/domain/',
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_POSTFIELDS => "domain=$keyword",
+        CURLOPT_POST => true,
+        CURLOPT_CONNECTTIMEOUT => 60
+    );
+    $ch = curl_init();
+    curl_setopt_array($ch, $setopt);
+    $exe = curl_exec($ch);
+    curl_close($ch);
+    return $exe;
+}
+
+if (!empty($_GET['key'])) {
+    $key = htmlspecialchars($_GET['key']);
+    $curl = curl($key);
+
+    // Scrap Web 
+    $regex = preg_match_all('/\s<a href=\"https:\/\/(.*?)\">/', $curl, $getkeyword);
+    $rep = array_filter(str_replace('teguh.co/', '', $getkeyword[1]));
+
+    // Rest Api
+    if (is_null($rep)) {
+        $api['status'] = 'failed';
+        $api['resutl'] = 'no result';
+        echo json_encode($api, JSON_PRETTY_PRINT);
+    } else {
+        $api['status'] = 'success';
+        $api['result'] = $rep;
+        echo json_encode($api, JSON_PRETTY_PRINT);
+    }
+} else {
+    $api['error'] = 'keyword empty!!';
+    echo json_encode($api, JSON_PRETTY_PRINT);
+}
