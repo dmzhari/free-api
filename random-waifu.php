9011a397cf8bfb765e7cee1a3c7919df4f518975
Dmz Hari <45622512+dmzhari@users.noreply.github.com>
2022-07-24 20:37:14 +0700

    Add files via upload

--- random-waifu.php
+++ random-waifu.php
@@ -1,19 +1,17 @@
 <?php
-
 header("Content-type: application/json");
 error_reporting(0);
 
-$url = file_get_contents('https://mywaifulist.moe/random');
-$name = preg_match('/<meta property=\"og:title\" content=\"(.*?)\">/', $url, $nametitle);
-$des = preg_match('/<meta property="og:description" content="(.*?)"/', $url, $description);
-$thumb = preg_match('/"image": "(.*?)"/', $url, $image);
-$gender = preg_match('/"gender": "(.*?)"/', $url, $gen);
+$url = file_get_contents('http://randomwaifu.altervista.org/');
+
+preg_match("/\">([a-zA-Z0-9].+\s[a-zA-Z0-9]+)/", $url, $nametitle);
+preg_match_all("/([a-zA-Z0-9]+.png)/", $url, $thumb);
+$split = explode(" from ", $nametitle[1]);
 
 $api = [
-    'name' => $nametitle[1],
-    'description' => htmlspecialchars_decode($description[1], ENT_QUOTES),
-    'thumbnail' => $image[1],
-    'gender' => $gen[1],
+    'name' => $split[0],
+    'anime' => $split[1],
+    'thumbnail' => "http://randomwaifu.altervista.org/images/" . $thumb[0][1],
 ];
 
 echo json_encode($api, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
